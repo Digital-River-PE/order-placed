a9b364523bf7ef071be92a836531684884ffa068
oiong <oiong@digitalriver.com>
2022-11-11 17:52:13 +0800

    output paymentData

--- react/OrderNumber.tsx
+++ react/OrderNumber.tsx
@@ -7,12 +7,13 @@ import { useOrder } from './components/OrderContext'
 const CSS_HANDLES = ['orderNumber']
 
 const OrderNumber = () => {
-  const { orderId } = useOrder()
+  const { orderId, paymentData } = useOrder()
   const handles = useCssHandles(CSS_HANDLES)
-
+   console.log();
   return (
     <h3 className={`${handles.orderNumber} mv0 t-heading-3 lh-copy`}>
       <FormattedMessage id="store/order.header.number" values={{ orderId }} />
+      <div>{`${JSON.stringify(paymentData)}`}</div>
     </h3>
   )
 }
