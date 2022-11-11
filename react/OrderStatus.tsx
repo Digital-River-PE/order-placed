e74a44246529fdcfb507d8e4625c3a29c3dd9033
oiong <oiong@digitalriver.com>
2022-11-11 21:09:35 +0800

    add order status

---
+++ react/OrderStatus.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import { useCssHandles } from 'vtex.css-handles'
+
+import { useOrder } from './components/OrderContext'
+
+const CSS_HANDLES = ['orderStatus']
+
+const OrderStatus = () => {
+  const { status } = useOrder()
+  const handles = useCssHandles(CSS_HANDLES)
+  return (
+    <small className={`${handles.orderStatus} c-muted-2 t-body lh-copy`}>
+      Order Status:
+      {
+        (status==='payment-pending')?(<>Pending</>):(<>In Process</>)
+      }
+      ( Original-{status} )
+    </small>
+  )
+}
+
+export default OrderStatus
